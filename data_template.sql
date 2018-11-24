e173cd5603174c55cbe743e758849354f07f7670
Tyler <tyleroalman@gmail.com>
2018-11-23 21:26:44 -0600

    fixed overlooked change

--- data_template.sql
+++ data_template.sql
@@ -7,8 +7,8 @@ INSERT INTO LESSON (l_id, l_type, duration, cost, course_id) VALUES
 INSERT INTO MANAGEMENT (mgmt_id, fname, lname, mgr_ssn) VALUES
 INSERT INTO PRODUCT_COLOR (product_id, color) VALUES
 INSERT INTO PRODUCT (p_id, p_name, p_group_name, for_gender, size_index, in_stock, sum_in_stock, wholesale_price, retail_price, date_received, quantity_received, location_received, vendor_id) VALUES
-INSERT INTO PURCHASE (p_id, customer_id, sales_person, p_date, payment_method) VALUES
-INSERT INTO PURCHASE_PRODUCT (purchase_id, product_id, num_items, total_price) VALUES
+INSERT INTO PURCHASE (p_id, customer_id, sales_person, p_date, payment_method, num_items, total_price) VALUES
+INSERT INTO PURCHASE_PRODUCT (purchase_id, product_id) VALUES
 INSERT INTO STUDENT_DETAIL (sd_id, fname, lname, sd_address, sd_city, sd_state, email, birth_date, phone_number) VALUES 
 INSERT INTO STUDENT (s_id, fname, lname, stud_detail_id) VALUES
 INSERT INTO TEACHER (t_id, employee_id) VALUES
