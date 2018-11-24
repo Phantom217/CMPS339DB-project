a62574237251dd78d891e907952f62f684370541
Benjamin <benjamin.henry@selu.edu>
2018-11-24 16:49:44 -0600

    added group practice and private remaining and taken inserts
    Names should match those in the code, I changed them back.  As always check though.

---
+++ GROUP_TAKEN.sql
@@ -0,0 +1,10 @@
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (1, 3, '7/28/2018', '68-672-1195', 76);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (2, 6, '5/27/2018', '65-613-2324', 18);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (3, 8, '6/18/2018', '66-651-8088', 140);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (5, 2, '9/26/2018', '92-189-1091', 78);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (9, 1, '8/10/2018', '25-244-2778', 5);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (1, 5, '6/5/2018', '86-028-0355', 63);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (4, 2, '10/6/2018', '41-948-5863', 96);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (8, 9, '8/8/2018', '44-727-6539', 55);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (1, 9, '10/27/2018', '72-013-6326', 23);
+insert into MOCK_DATA (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) values (5, 8, '8/30/2018', '75-316-1844', 54);
---
+++ PRACTICE_TAKEN.sql
@@ -0,0 +1,10 @@
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (10, 3, '7/19/2018', '78-516-9349', 61);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (10, 4, '3/20/2018', '60-287-2516', 25);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (9, 10, '3/27/2018', '31-047-4846', 16);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (10, 10, '2/22/2018', '47-611-5672', 21);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (3, 7, '4/30/2018', '26-332-4582', 92);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (5, 10, '3/5/2018', '45-738-3784', 128);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (2, 9, '10/21/2018', '15-862-8511', 145);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (1, 10, '3/10/2018', '39-543-2554', 94);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (10, 3, '1/8/2018', '51-610-1914', 41);
+insert into MOCK_DATA (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) values (3, 8, '3/18/2018', '46-759-6754', 77);
