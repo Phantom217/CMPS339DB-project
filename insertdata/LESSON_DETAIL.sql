4de4148645e7c8e16f833ade13940eefaad55fae
Tyler <tyleroalman@gmail.com>
2018-11-24 18:24:51 -0600

    added lesson_detail

--- insertdata/LESSON_DETAIL.sql
+++ insertdata/LESSON_DETAIL.sql
@@ -1,1 +1,6 @@
-INSERT INTO LESSON_DETAIL (ld_id, couse+id, lesson_id, duration) VALUES
+INSERT INTO LESSON_DETAIL (ld_id, couse_id, lesson_id, duration) VALUES (1,1,1,01:30:00),
+(2,1,3,01:00:00),
+(3,4,12,01:30:00),
+(4,1,3,01:00:00),
+(5,3,6,01:30:00),
+(6,1,1,01:00:00);
