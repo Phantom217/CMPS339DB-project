0e92adc85a5bf114b98d9c04312224ad6185e7d3
Tyler <tyleroalman@gmail.com>
2018-11-24 18:27:48 -0600

    added lesson_taken

--- insertdata/LESSON_TAKEN.sql
+++ insertdata/LESSON_TAKEN.sql
@@ -1,1 +1,4 @@
-INSERT INTO LESSON_TAKEN (lt_id, appt_id, course_id, lesson_id) VALUES
+INSERT INTO LESSON_TAKEN (lt_id, appt_id, course_id, lesson_id) VALUES (1,1,1,1),
+(2,2,1,3),
+(3,3,4,12),
+(4,5,3,6);
