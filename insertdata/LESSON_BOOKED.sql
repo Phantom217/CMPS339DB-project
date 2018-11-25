779078f1c2bc891f6aa510150e51ac2c7c46f301
Tyler <tyleroalman@gmail.com>
2018-11-24 18:15:56 -0600

    added lesson_booked

--- insertdata/LESSON_BOOKED.sql
+++ insertdata/LESSON_BOOKED.sql
@@ -1,1 +1,6 @@
-INSERT INTO LESSON_BOOKED (lb_id, appt_id, course_id, lesson_id) VALUES
+INSERT INTO LESSON_BOOKED (lb_id, appt_id, course_id, lesson_id) VALUES (1,1,1,1),
+(2,2,1,3),
+(3,3,4,12),
+(4,4,1,3),
+(5,5,3,6),
+(6,6,1,1);
