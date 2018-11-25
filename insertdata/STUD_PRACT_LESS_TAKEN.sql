a23d177fdc74ff4ca8edda17e05e86fcffddbfa6
Tyler <tyleroalman@gmail.com>
2018-11-24 21:05:41 -0600

    added pract_less_taken data

--- insertdata/STUD_PRACT_LESS_TAKEN.sql
+++ insertdata/STUD_PRACT_LESS_TAKEN.sql
@@ -1,1 +1,6 @@
-INSERT INTO STUD_PRACT_LESS_TAKEN (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) VALUES
+INSERT INTO STUD_PRACT_LESS_TAKEN (spctlt_id, spctlr_id, sheduled_pract_date, lesson_taken_id, tcpctt) VALUES (1,1,NULL,1,0),
+(2,2,2018-11-20 13:00:00,2,1),
+(3,3,2018-11-20 11:00:00,3,1),
+(4,4,2018-11-20 13:00:00,4,0),
+(5,5,2018-11-20 11:00:00,5,1),
+(6,6,NULL,6,0);
