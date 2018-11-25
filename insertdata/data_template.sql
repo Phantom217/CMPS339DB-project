fd60c3c7182c888d7fddaac54d08a2e61a36cfad
Tyler <tyleroalman@gmail.com>
2018-11-24 18:50:36 -0600

    removed data_template as all templates in it have been used now

--- insertdata/data_template.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ENROLLMENT_DETAIL (ed_id, enroll_id, student_id, course_id, priv_course, group_course, pract_course, supervisor_id, teacher_id, enroll_date) VALUES 
-INSERT INTO ENROLLMENT (e_id, e_date, mgmt_id) VALUES
