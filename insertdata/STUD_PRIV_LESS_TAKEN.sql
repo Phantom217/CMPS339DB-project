01e48daef62e1a3f5a5e38fd7729f79e21d879b9
Tyler <tyleroalman@gmail.com>
2018-11-24 21:09:51 -0600

    added priv_less_taken data

--- insertdata/STUD_PRIV_LESS_TAKEN.sql
+++ insertdata/STUD_PRIV_LESS_TAKEN.sql
@@ -1,1 +1,6 @@
-INSERT INTO STUD_PRIV_LESS_TAKEN (sprvlt_id, sprvlr_id, sheduled_priv_date, lesson_taken_id, tcprvt) VALUES
+INSERT INTO STUD_PRIV_LESS_TAKEN (sprvlt_id, sprvlr_id, sheduled_priv_date, lesson_taken_id, tcprvt) VALUES (1,1,2018-11-20 11:00:00,1,1),
+(2,2,NULL,2,0),
+(3,3,NULL,3,0),
+(4,4,NULL,4,0),
+(5,5,NULL,5,0),
+(6,6,2018-11-20 13:00:00,6,0);
