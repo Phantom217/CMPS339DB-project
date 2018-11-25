95b0f90e562cc1180a77b3b20cbf00783c69b77b
Tyler <tyleroalman@gmail.com>
2018-11-24 20:47:21 -0600

    added group_less_remain data

--- insertdata/STUD_GROUP_LESS_REMAIN.sql
+++ insertdata/STUD_GROUP_LESS_REMAIN.sql
@@ -1,1 +1,6 @@
-INSERT INTO STUD_GROUP_LESS_REMAIN (sglr_id, sglt, student_id, enroll_detail_id, tcgr) VALUES
+INSERT INTO STUD_GROUP_LESS_REMAIN (sglr_id, sglt_id, student_id, enroll_detail_id, tcgr) VALUES (1,1,4,1,10),
+(2,2,6,2,10),
+(3,3,7,3,10),
+(4,4,14,4,10),
+(5,5,64,5,10),
+(6,6,37,6,10);
