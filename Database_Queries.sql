3d554d0b3bda9266df4855d3d46d1a3f88eb09ce
Chiblesworth <dustin.branch@selu.edu>
2018-11-25 13:38:13 -0800

    Update Database_Queries.sql

--- Database_Queries.sql
+++ Database_Queries.sql
@@ -3,14 +3,7 @@
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 --DDL QUERIES (THERE MUST BE 10 DDL QUERIES; GIVE A NUMBER "#" TO EACH QUERY)
---Q1: CREATE TABLE ...
--- Create table named SIZE
-CREATE TABLE SIZE(
-	size_id			INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	size_num		FLOAT(2,1),
-	size_char		VARCHAR(3) CHECK (size_char = 'XS' OR size_char = 'S' OR size_char = 'M' OR size_char = 'L' OR size_char = 'XL' OR size_char = 'XXL')
-
-);
+--Q#: CREATE TABLE ...
 
 
 --Q2: ALTER TABLE ...
@@ -33,12 +26,7 @@ FOREIGN KEY (size_index) REFERENCES SIZE(size_id);
 
 --Q#: ADD TRIGGER TO CHECK ...
 AIM: .....................
-/**
- *
- *  something like:
- *  CONSTRAINT CHK_Sizes CHECK ((size_int NOT NULL AND size_char IS NULL) OR (size_int IS NULL AND size_char NOT NULL)) + TRIGGER
- *
- */
+
 
 
 
@@ -57,8 +45,12 @@ AIM: .....................
 AIM: .....................
 
 
---Q#: UPDATE DATA:
-AIM: .....................
+--Q3: UPDATE DATA:
+-- Add 1 the each d_num column in the employee table, after using IDENTITY
+-- We found that some values in our data was incremented strangly. This was a
+-- Quick solution to the problem.
+UPDATE EMPLOYEE
+SET d_num=d_num+1;
 
 
 
@@ -68,17 +60,16 @@ AIM: .....................
 
 
 --Q5: QUERY DATA:
--- See all appointments for bronze, silver, and gold courses. (course_id > 2)
---Just using something very basic can be changed
+-- Determine how many appointments were made for the Social Foundation course.(course_id = 2)
 SELECT *
 FROM APPOINTMENT
-WHERE course_id > 2
+WHERE course_id = 2;
 
 --Q6: QUERY DATA WITH SUB-QUERY IN WHERE CLAUSE:
 -- Find product that exists under the group name accessory
 -- Example: https://www.essentialsql.com/get-reay-tdo-learn-sql-server-21-using-subqueries-in-the-where-clause/
-SELECT p_id, p_name, retail_price
-FROM PRDOUCT AS P
+SELECT P.p_id, P.p_name, P.retail_price, P.sum_in_stock
+FROM PRODUCT AS P
 WHERE EXISTS (SELECT P.p_group_name, P.sum_in_stock
 			  FROM PRODUCT
 			  WHERE P.p_group_name = 'accessories' AND P.sum_in_stock >= 1
@@ -106,57 +97,69 @@ FROM EMPLOYEE AS E
 WHERE E.job_title = 'Janitor';
 
 --Q10: QUERY DATA WITH ANY/SOME/ALL:
--- Return rows in PRODUCT where sum_in_stock is greater than 10
+-- Return rows in PRODUCT where total number of items in stock is greater than 10.
 SELECT p_id, p_name, sum_in_stock
 FROM PRODUCT
-WHERE p_id = ALL (SELECT p_id FROM PRODUCT WHERE sum_in_stock >= 10)
+WHERE p_id = ANY (SELECT p_id FROM PRODUCT WHERE sum_in_stock > 10);
+
+
 --Q11: INNER-JOIN-QUERY:
--- Join PRODUCT and VENDOR table
--- Maybe this will work? 
+-- Determine which vendors supply each product. 
 --Example: https://www.w3schools.com/sql/sql_join_inner.asp
 SELECT P.p_id, P.p_name, V.v_id, V.v_name
-FROM PRODUCT AS P, VENDOR AS V
-INNER JOIN V ON  P.vendor_id = V.v_id
+FROM PRODUCT AS P
+INNER JOIN  VENDOR AS V ON  P.vendor_id = V.v_id;
 
 --Q12: FULL-OUTER-JOIN-QUERY:
--- See all lessons that were booked and taken.
+-- See all lessons that were booked and then determine which lessons were taken that day.
 -- Some useful links: https://stackoverflow.com/questions/2094793/when-is-a-good-situation-to-use-a-full-outer-join
 -- https://www.w3schools.com/sql/sql_join_full.asp
 SELECT B.appt_id, B.course_id, B.lesson_id, T.course_id, T.lesson_id
-FROM LESSON_BOOKED AS B, LESSON_TAKEN AS T
-FULL OUTER JOIN T ON B.appt_id = T.appt_id;
+FROM LESSON_BOOKED AS B
+FULL OUTER JOIN LESSON_TAKEN AS T ON B.appt_id = T.appt_id
 ORDER BY B.appt_id;
 
+
 --Q13#: LEFT-OUTER-JOIN-QUERY:
--- See all products bought for a particular purchase
+-- See all products bought for a particular purchase.
 -- Example: https://www.w3schools.com/sql/sql_join_left.asp
 SELECT *
-FROM PURCHASE AS P, 
+FROM PURCHASE AS P
 LEFT JOIN PURCHASE_PRODUCT AS PP ON P.p_id = PP.purchase_id
 ORDER BY P.p_id;
 
 --Q14: RIGHT-OUTER-JOIN-QUERY:
--- Get the first name, last name, and managment ID
+-- Get the first name, last name, and managment ID of employees that are teachers.
 -- Link to where I found example: https://www.w3schools.com/sql/sql_join_right.asp
-SELECT E.employee_id, T.t_id, E.fname, E.lname, E.mgmt_id
-FROM TEACHER AS T
-RIGHT JOIN EMPLOYEE AS E ON T.employee_id = E.e_id
+SELECT T.t_id, E.e_id, E.fname, E.lname, E.mgmt_id
+FROM EMPLOYEE AS E
+RIGHT JOIN TEACHER AS T ON E.e_id = T.employee_id
 ORDER BY T.t_id;
 
 
 --Q15 AGGREGATION-JOIN-QUERY WITH GROUP BY & HAVING:
--- NOT DONE LOOKING AT IT NOW
+-- Determine the total number of sales from each individual cashier
+-- Links: https://www.w3schools.com/sql/sql_having.asp
+-- https://www.w3schools.com/sql/sql_groupby.asp
+SELECT E.e_id, E.lname, COUNT(P.sales_person) AS num_of_sales
+FROM PRUCHASE AS P
+INNER JOIN EMPLOYEE AS E ON P.sales_person = E.e_id
+GROUP BY E.e_id
+HAVING COUNT(P.sales_person) > 1;
 
 
+--Q16: AGGREGATION-JOIN-QUERY WITH SUB-QUERY:
+-- Determine number of employees in a particular department
+SELECT D.d_num, COUNT(E.d_num) AS total_employees
+FROM DEPARTMENT AS D
+FULL OUTER JOIN EMPLOYEE AS E ON D.d_num = E.d_num
+WHERE EXISTS (SELECT E.d_num
+			  FROM EMPLOYEE
+			  WHERE E.d_num >= 2
+			)
+GROUP BY D.d_num;
 
---Q#: AGGREGATION-JOIN-QUERY WITH SUB-QUERY:
-AIM: .....................
-
-
-
---Q#: WITH-QUERY:
-AIM: .....................
-
+--Q17: WITH-QUERY:
 
 
 
