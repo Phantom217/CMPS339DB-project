7a03b92e4928cc17372aebd8e88b1008dd928231
Tyler <tyleroalman@gmail.com>
2018-11-25 00:38:17 -0600

    modified queries to work in ssms

--- insertdata/MANAGEMENT.sql
+++ insertdata/MANAGEMENT.sql
@@ -1,6 +1,6 @@
-INSERT INTO MANAGEMENT (mgmt_id, fname, lname, mgr_ssn) VALUES (1,'Fulton','Conman','709-32-9489'),
-(2,'Creigh','Seabright','126-55-0901'),
-(3,'Dallas','Kinchley','711-88-2567'),
-(4,'Shantee','Mattimoe','407-95-5221'),
-(5,'Andre','Tottie','632-41-4276');
+INSERT INTO MANAGEMENT (mgmt_id, fname, lname, mgr_ssn) VALUES ('Fulton','Conman','709-32-9489'),
+('Creigh','Seabright','126-55-0901'),
+('Dallas','Kinchley','711-88-2567'),
+('Shantee','Mattimoe','407-95-5221'),
+('Andre','Tottie','632-41-4276');
 
