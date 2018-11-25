1ca587957ae6b7052369b7c5dd391623961a2b38
Tyler <tyleroalman@gmail.com>
2018-11-24 20:39:43 -0600

    added priv_less_remain data

---
+++ insertdata/STUD_PRIV_LESS_REMAIN.sql
@@ -0,0 +1,6 @@
+INSERT INTO STUD_PRIV_LESS_REMAIN (sprvlr_id, sprvlt_id, student_id, enroll_detail_id, tcprvr) VALUES (1,1,4,1,9),
+(2,2,6,2,10),
+(3,3,7,3,10),
+(4,4,14,4,10),
+(5,5,64,5,10),
+(6,6,37,6,9);
