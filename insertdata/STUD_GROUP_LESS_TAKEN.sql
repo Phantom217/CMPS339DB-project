55a4f4f37ca18392ace52eb0b8696ca0086436a7
Tyler <tyleroalman@gmail.com>
2018-11-24 21:12:17 -0600

    added group_less_taken data

--- insertdata/STUD_GROUP_LESS_TAKEN.sql
+++ insertdata/STUD_GROUP_LESS_TAKEN.sql
@@ -1,1 +1,6 @@
-INSERT INTO STUD_GROUP_LESS_TAKEN (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) VALUES
+INSERT INTO STUD_GROUP_LESS_TAKEN (sglt, sglr_id, sheduled_group_date, lesson_taken_id, tcgt) VALUES (1,1,NULL,1,0),
+(2,2,NULL,2,0),
+(3,3,NULL,3,0),
+(4,4,NULL,4,0),
+(5,5,NULL,5,0),
+(6,6,NULL,6,0);
